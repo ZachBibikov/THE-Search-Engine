b15c0622f97255c9855de3e7e2d55ae0f8f24c49
Zach <122324135+ZachBibikov@users.noreply.github.com>
2023-05-09 14:02:09 -1000

    Add files via upload

---
+++ Projects/WebsiteHolder/src/WebsiteHolder.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class WebsiteHolder {
+    public static void main(String[] args) {
+
+        String makeAWebsite = "So, you want to build a website right? Click the link and a website will be built for you" +
+                "    ITS DEFINETLY NOT A VIRUS";
+
+        System.out.println(makeAWebsite);
+
+
+        }
+    }
